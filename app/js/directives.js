349ee86c7b644e07a5abf87a2148d93852d8794f
xiaohan <xiaohan2012@gmail.com>
2013-08-07 19:00:27 +0800

    jq-slider min,max,step,defaultVal added

--- app/js/directives.js
+++ app/js/directives.js
@@ -17,7 +17,7 @@ angular.module('veWeb.directives', []).
 	}
     })
     .controller("jqSliderCtrl", function($scope){
-	$scope.threshold = 0;
+	$scope.threshold = 5;
     })
     .directive("jqSlider", function($parse){
 	return {
@@ -25,27 +25,33 @@ angular.module('veWeb.directives', []).
 	    replace: true,
 	    transclude: false,
 	    compile: function (element, attrs) {
-		var modelAccessor = $parse(attrs.ngModel);
-
-		var html = '<div class="span3" style=""></div><span class="help-inline">{{threshold}}</span>'
+		var modelAccessor = $parse(attrs.ngModel),
+		min = attrs.min ? parseFloat(attrs.min) : 0, 
+		max = attrs.max ? parseFloat(attrs.max) : 10,
+		step = attrs.step ? parseFloat(attrs.step) : 0.1,
+		defaultVal = attrs.defaultVal ? parseFloat(attrs.defaultVal) : 4;
 		
-
+		var html = '<div class="span3 slider" ></div><span class="help-inline">{{threshold}}</span>'
+		
 		var newElem = $(html);
 		element.replaceWith(newElem);
 
 		return function (scope, element, attrs, controller) {
 		    element.slider({
+			min: min, 
+			max: max,
+			step: step,
 			slide: function(event, ui){
 			    if(!scope.$$phase){
 				scope.$apply(function (scope) {
 				    modelAccessor.assign(scope, ui.value);
-				    console.log(scope.threshold);
 				});
 			    }
 			}
 		    });
-		    
 
+		    scope.threshold = defaultVal;
+		    
 		    scope.$watch(modelAccessor, function (val) {
 			element.slider({"value": val});
 		    });
